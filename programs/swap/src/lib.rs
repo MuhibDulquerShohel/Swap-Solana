92e5fcff49e25f7d477a297186f31bed058ff056
Muhib Dulquer Shohel <muhibdulquershohel@gmail.com>
2024-11-15 23:25:08 +0600

    implementing swap program

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("6dFP2EhzhuX4dh39EtR5rzejDeKqVk6vRVWQzWwZ6fB7");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
